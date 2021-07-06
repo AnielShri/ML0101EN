31f4df89daadfe775c992a85dc7570929690220a
Aniel - Nitro <aniel.shri@outlook.com>
2021-07-06 17:44:19 +0200

    Added config files

---
+++ winipython_ml.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo Launcher for Jupyter notebooks using Winpython
+
+set WINPYTHON_DIR=%USERPROFILE%\Downloads\dev\python\WPy64-3950
+
+call "%WINPYTHON_DIR%\scripts\env_for_icons.bat" %*
+cd/D "%WINPYWORKDIR1%"
+
+rem "%WINPYDIR%\scripts\jupyter-notebook.exe" --notebook-dir=%~dp0
+
+"%WINPYDIR%\scripts\jt.exe" -t chesterish -T -N -f source -nf source -nfs 12 -cellw 1200 -N -kl -cursc r -T 
+
+"%WINPYDIR%\scripts\jupyter-notebook.exe" --notebook-dir=%~dp0 --no-browser
+
+pause
