249933e3480fd923b8f855b646778e9c2e106259
Aniel - Nitro <aniel.shri@outlook.com>
2021-07-06 18:18:14 +0200

    M02L01 completed

--- winipython_ml.bat
+++ winipython_ml.bat
@@ -7,8 +7,6 @@ set WINPYTHON_DIR=%USERPROFILE%\Downloads\dev\python\WPy64-3950
 call "%WINPYTHON_DIR%\scripts\env_for_icons.bat" %*
 cd/D "%WINPYWORKDIR1%"
 
-rem "%WINPYDIR%\scripts\jupyter-notebook.exe" --notebook-dir=%~dp0
-
 "%WINPYDIR%\scripts\jt.exe" -t chesterish -T -N -f source -nf source -nfs 12 -cellw 1200 -N -kl -cursc r -T 
 
 "%WINPYDIR%\scripts\jupyter-notebook.exe" --notebook-dir=%~dp0 --no-browser
