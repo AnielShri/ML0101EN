c2c90c608ffae7c5df453985b57a886d03df7056
Aniel - Nitro <aniel.shri@outlook.com>
2021-07-09 19:34:27 +0200

    diff types of linear

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ML0101EN
 Machine Learning with Python
+
+
+Resources: https://courses.cognitiveclass.ai/courses/course-v1:BigDataUniversity+ML0101EN+2016/e985f659bdcc4e339f4a481611b3e0fc/
+
--- winipython_ml.bat
+++ winipython_ml.bat
@@ -7,7 +7,7 @@ set WINPYTHON_DIR=%USERPROFILE%\Downloads\dev\python\WPy64-3950
 call "%WINPYTHON_DIR%\scripts\env_for_icons.bat" %*
 cd/D "%WINPYWORKDIR1%"
 
-"%WINPYDIR%\scripts\jt.exe" -t chesterish -T -N -f source -nf source -nfs 12 -cellw 1200 -N -kl -cursc r -T 
+rem "%WINPYDIR%\scripts\jt.exe" -t chesterish -T -N -f source -nf source -nfs 12 -cellw 1200 -N -kl -cursc r -T 
 
 "%WINPYDIR%\scripts\jupyter-notebook.exe" --notebook-dir=%~dp0 --no-browser
 
